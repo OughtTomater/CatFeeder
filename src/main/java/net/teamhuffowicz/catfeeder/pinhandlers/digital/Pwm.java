dc80701b4196861167eb831ee89b561757772f48
OughtTomater <30742080+OughtTomater@users.noreply.github.com>
2018-01-27 20:10:02 -0500

    changed to pi4j example

--- src/main/java/net/teamhuffowicz/catfeeder/pinhandlers/digital/Pwm.java
+++ src/main/java/net/teamhuffowicz/catfeeder/pinhandlers/digital/Pwm.java
@@ -1,18 +1,23 @@
 package net.teamhuffowicz.catfeeder.pinhandlers.digital;
 
 import com.pi4j.io.gpio.*;
+import com.pi4j.util.CommandArgumentParser;
 import com.pi4j.wiringpi.Gpio;
 
 public class Pwm {
 
-   private final GpioController gpioFactory = GpioFactory.getInstance();
+   private final GpioController gpio = GpioFactory.getInstance();
+
+   Pin pin = CommandArgumentParser.getPin(RaspiPin.class, RaspiPin.GPIO_01);
+
+    GpioPinPwmOutput pwm = gpio.provisionPwmOutputPin(pin);
 
    public void turnServo(){
        Gpio.pinMode(18,Gpio.PWM_OUTPUT);
        Gpio.pwmSetMode(Gpio.PWM_MODE_MS);
-       Gpio.pwmSetClock(384);
+       Gpio.pwmSetClock(500);
        Gpio.pwmSetRange(1000);
-       Gpio.pwmWrite(18,75);
+       pwm.setPwm(500);
    }
 
    /*public GpioPinPwmOutput createServo(String pin){
